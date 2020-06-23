9444c19a9553766edf7a9fd1f528ac0be3405fc6
Carlos Villanueva Ousset <crvo84@gmail.com>
2020-06-22 23:57:56 -0500

    Initial commit.

---
+++ GoalsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalsApp
+//
+//  Created by Carlos Villanueva Ousset on 22/06/20.
+//  Copyright © 2020 Villou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

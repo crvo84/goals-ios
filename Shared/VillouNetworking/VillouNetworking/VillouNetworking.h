eed7cd9dd93acafd4925002dda091a1175907a9f
Carlos Villanueva Ousset <crvo84@gmail.com>
2020-06-25 17:14:35 -0500

    Create VillouNetworking shared project.

---
+++ Shared/VillouNetworking/VillouNetworking/VillouNetworking.h
@@ -0,0 +1,19 @@
+//
+//  VillouNetworking.h
+//  VillouNetworking
+//
+//  Created by Carlos Villanueva Ousset on 25/06/20.
+//  Copyright © 2020 Villou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VillouNetworking.
+FOUNDATION_EXPORT double VillouNetworkingVersionNumber;
+
+//! Project version string for VillouNetworking.
+FOUNDATION_EXPORT const unsigned char VillouNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VillouNetworking/PublicHeader.h>
+
+
---
+++ Shared/VillouNetworking/VillouNetworkingTests/VillouNetworkingTests.swift
@@ -0,0 +1,34 @@
+//
+//  VillouNetworkingTests.swift
+//  VillouNetworkingTests
+//
+//  Created by Carlos Villanueva Ousset on 25/06/20.
+//  Copyright © 2020 Villou. All rights reserved.
+//
+
+import XCTest
+@testable import VillouNetworking
+
+class VillouNetworkingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

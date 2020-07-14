2c426db64793799325b5de507858153dc1d99f62
Carlos Villanueva Ousset <crvo84@gmail.com>
2020-07-14 15:49:28 -0500

    Create VillouSecureStore shared project.

---
+++ Shared/VillouSecureStore/VillouSecureStore/VillouSecureStore.h
@@ -0,0 +1,19 @@
+//
+//  VillouSecureStore.h
+//  VillouSecureStore
+//
+//  Created by Carlos Villanueva Ousset on 14/07/20.
+//  Copyright © 2020 Villou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VillouSecureStore.
+FOUNDATION_EXPORT double VillouSecureStoreVersionNumber;
+
+//! Project version string for VillouSecureStore.
+FOUNDATION_EXPORT const unsigned char VillouSecureStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VillouSecureStore/PublicHeader.h>
+
+
